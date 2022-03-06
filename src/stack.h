8cdec74b3978658beb480f22a831f39660974980
Veeta Baratheon <veetabar@va-c5.kzn.21-school.ru>
2022-03-06 13:20:51 +0300

    stack.h with cpplint

---
+++ src/stack.h
@@ -0,0 +1,13 @@
+#ifndef SRC_STACK_H_
+#define SRC_STACK_H_
+
+// помещает число в стэк, возвращает добавленное значение
+double add_queue(double number);
+
+// получение последнего значения из стэка
+double take_queue();
+
+// очищение памяти стэка
+void kill_queue();
+
+#endif  // SRC_STACK_H_
